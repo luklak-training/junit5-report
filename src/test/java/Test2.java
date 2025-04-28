92300312d2d762315513132b535169baf054dc85
thanh5320 <thanh20183632@gmail.com>
2025-04-28 16:14:53 +0700

    update jkf

---
+++ src/test/java/Test2.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Test2 {
+    @Test
+    void t1(){
+        Assertions.assertEquals(1,1);
+    }
+
+    @Test
+    void t2(){
+        Assertions.assertEquals(1,1);
+    }
+}
