edf99e53de2acc5fc0683b44b0da12cbbbfdd398
thanh5320 <thanh20183632@gmail.com>
2025-05-07 11:08:07 +0700

    test

---
+++ app/src/test/java/app/Test1.java
@@ -0,0 +1,17 @@
+package app;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Test1 {
+
+//    @Test
+//    void t1(){
+//        Assertions.assertEquals(1, 1);
+//    }
+
+    @Test
+    void t2(){
+        Assertions.assertEquals(2, 1);
+    }
+}
